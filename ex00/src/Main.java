38bcfaf8fb4c8ab57e582d1fa1f6965537ceae0d
굽인이 <97422685+Gyuub@users.noreply.github.com>
2022-01-25 15:20:20 +0900

    ex00/src
    gitignore 테스트

--- ex00/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Tiger tiger = new Tiger(new Animal(), "타이거");
-        System.out.println(tiger.name);
-        System.out.println(tiger.animal.name);
-    }
-}
--- ex00/src/Tiger.java
+++
@@ -1,9 +0,0 @@
-public class Tiger{
-    Animal animal;
-    public String name = "타이거";
-
-    public Tiger(Animal animal, String name) {
-        this.animal = animal;
-        this.name = name;
-    }
-}
